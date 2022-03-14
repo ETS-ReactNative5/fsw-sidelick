17952f72e0c32e13d1813ce2102b15c31fabd372
Hala Zbib <halazbib22@gmail.com>
2022-03-14 02:41:05 +0200

    trying to link the button and hypertext to other pages

--- App.js
+++ App.js
@@ -1,8 +1,8 @@
 import react from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import OnBoardingScreen from './app/screens/components/OnBoardingScreen';
-import SignUp from './app/screens/components/SignUp';
-import SignIn from './app/screens/components/SignIn';
+// import SignUp from './app/screens/components/SignUp';
+// import SignIn from './app/screens/components/SignIn';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 const Stack = createNativeStackNavigator();
 
@@ -11,8 +11,8 @@ export default function App() {
     <NavigationContainer>
       <Stack.Navigator screenOptions={{headerShown: false}}>
         <Stack.Screen name="OnBoardingScreen" component={OnBoardingScreen}/>
-        <Stack.Screen name="SignUp" component={SignUp}/>
-        <Stack.Screen name="SignIn" component={SignIn}/>
+        {/* <Stack.Screen name="SignUp" component={SignUp}/> */}
+        {/* <Stack.Screen name="SignIn" component={SignIn}/> */}
       </Stack.Navigator>
     </NavigationContainer>
    )
--- app/screens/components/OnBoardingScreen.js
+++ app/screens/components/OnBoardingScreen.js
@@ -10,13 +10,13 @@ import {
   Image,
   Text,
   Button,
+  Pressable,
 } from "react-native";
-import { BlurView } from 'expo-blur';
+import { BlurView } from "expo-blur";
 import JoinButton from "./JoinButton";
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import SignIn from './SignIn';
-
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import SignIn from "./SignIn";
 const Stack = createNativeStackNavigator();
 
 const { width, height } = Dimensions.get("window");
@@ -25,43 +25,53 @@ const COLORS = { Primary: "#ff8400", white: "#fff" };
 
 const OnBoardingScreen = ({ navigation }) => {
   return (
-    <ImageBackground style={{flex:1, width, height }} source={require("../assets/dog-walker.png")}>
+    <ImageBackground
+      style={{ flex: 1, width, height }}
+      source={require("../assets/dog-walker.png")}
+    >
       <View style={styles.container}>
-      <BlurView intensity={15} tint="light" style={styles.blurContainer}>
-        <Text style={styles.text}>Too tired to walk your dog?</Text>
-        <Text style={styles.text}>Let us help you!</Text>
-        <JoinButton/>
-        <Text style={styles.subtext}>Already a member?
-        </Text>
-      </BlurView>
+        <BlurView intensity={15} tint="light" style={styles.blurContainer}>
+          <Text style={styles.text}>Too tired to walk your dog?</Text>
+          <Text style={styles.text}>Let us help you!</Text>
+          <JoinButton />
+          <View>
+            <Text style={styles.subtext}>
+              Already a member?
+              {/* <Text style={{color: 'orange'}} onPress={() => navigation.navigate("SignIn")}> Sign In</Text> */}
+              <Pressable onPress={() => navigation.navigate("SignIn")}>
+                <Text style={{ color: "orange" }}> Sign In</Text>
+              </Pressable>
+            </Text>
+          </View>
+        </BlurView>
       </View>
     </ImageBackground>
   );
 };
 
 const styles = StyleSheet.create({
-  container:{
-    flex:1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    marginTop: height * 0.3 ,
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: height * 0.3,
   },
-	text:{
-		color: COLORS.white,
-		fontSize: 22,
-		fontWeight: "bold",
+  text: {
+    color: COLORS.white,
+    fontSize: 22,
+    fontWeight: "bold",
     letterSpacing: 1,
-		marginTop: 5,
-		textAlign: 'center',
-	},
-	subtext:{
-		color: COLORS.white,
-		fontSize: 14,
+    marginTop: 5,
+    textAlign: "center",
+  },
+  subtext: {
+    color: COLORS.white,
+    fontSize: 14,
     letterSpacing: 0.5,
-		textAlign: 'center',
-	},
-  blurContainer:{
-    paddingVertical:20,
+    textAlign: "center",
+  },
+  blurContainer: {
+    paddingVertical: 20,
     paddingHorizontal: 10,
     width: "80%",
   },
