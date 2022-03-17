57d0d5cdfeb31d733e31be09c1645c65c92e4d81
Hala Zbib <halazbib22@gmail.com>
2022-03-17 20:50:09 +0200

    created routes file and imported it to the app.js

--- App.js
+++ App.js
@@ -1,27 +1,13 @@
-import react from 'react';
-import OnBoardingScreen from './app/screens/components/OnBoardingScreen';
-import SignUp from './app/screens/components/SignUp';
-import SignIn from './app/screens/components/SignIn';
+import Routes from './app/Routes/Routes';
 
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-const Stack = createNativeStackNavigator();
-
-function App() {
+const App = () => {
   return (
-      <Stack.Navigator screenOptions={{headerShown: false}}>
-        <Stack.Screen name="OnBoardingScreen" component={OnBoardingScreen}/>
-        <Stack.Screen name="SignUp" component={SignUp} />
-        <Stack.Screen name="SignIn" component={SignIn}/>
-      </Stack.Navigator>
+      <>
+        <Routes/>
+      </>
    )
   };
 
-export default () => {
-  return (
-    <NavigationContainer>
-      <App/>
-    </NavigationContainer>
-  )
-}
+export default App;
+
 
---
+++ app/Routes/Routes.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import SignIn from "../screens/components/SignIn";
+import SignUp from "../screens/components/SignUp";
+import OnBoardingScreen from "../screens/components/OnBoardingScreen";
+
+const Routes = () => {
+  const Stack = createNativeStackNavigator();
+  return (
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{headerShown: false}}>
+        <Stack.Screen name="OnBoardingScreen" component={OnBoardingScreen} />
+        <Stack.Screen name="SignUp" component={SignUp} />
+        <Stack.Screen name="SignIn" component={SignIn} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default Routes;
