0979ed439db43671fa88c80dc2b2f4e1009ab97c
Hala Zbib <halazbib22@gmail.com>
2022-03-20 15:10:00 +0200

    working on the navigation bar

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
 import Routes from './app/Routes/Routes';
+import NavBar from "./app/screens/components/ReusableComponents/NavBar";
 
 const App = () => {
   return (
       <>
-        <Routes/>
+        {/* <Routes/> */}
+      <NavBar/>
       </>
    )
   };
