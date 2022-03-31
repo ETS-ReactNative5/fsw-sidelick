5dc73cdff43fec0427b55c3d5317f6e78c56c78b
Hala Zbib <halazbib22@gmail.com>
2022-03-31 21:48:52 +0300

    working on image upload

--- App.js
+++ App.js
@@ -15,19 +15,22 @@ import Tabs from "./app/Routes/Tabs";
 import WalkerProfile from "./app/screens/components/WalkerProfile";
 import EditProfile from "./app/screens/components/EditProfile";
 import EditWalkerProfile from "./app/screens/components/EditWalkerProfile";
+import ImageUpload from "./app/screens/ReusableComponents/ImageUpload";
 
 const App = () => {
   const Stack = createNativeStackNavigator();
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="OnBoardingScreen" screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="OnBoardingScreen" component={OnBoardingScreen} />
+        {/* <Stack.Screen name="OnBoardingScreen" component={OnBoardingScreen} />
          <Stack.Screen name="SignUp" component={SignUp} />
         <Stack.Screen name="SignIn" component={SignIn} /> 
          <Stack.Screen name="Tabs" component={Tabs} />
-        <Stack.Screen name="EditProfile" component={EditProfile} /> 
+        <Stack.Screen name="EditProfile" component={EditProfile} />  */}
         {/* <Stack.Screen name="EditWalkerProfile" component={EditWalkerProfile} /> */}
-        <Stack.Screen name="WalkerProfile" component={WalkerProfile} />
+        {/* <Stack.Screen name="WalkerProfile" component={WalkerProfile} /> */}
+        <Stack.Screen name="ImageUpload" component={ImageUpload} />
+
       </Stack.Navigator>
     </NavigationContainer>
   );
