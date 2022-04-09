3594555e8f07a6e5dc91842aa1183b0032030666
Hala Zbib <halazbib22@gmail.com>
2022-04-09 21:11:18 +0300

    const verifyToken

--- SideLick--Backend/routes/verifyToken.js
+++ SideLick--Backend/routes/verifyToken.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 
 // middleware function that we can add to routes that we want to be protected
-module.exports = function(req, res, next){
+const verifyToken = (req, res, next) => {
 	const token = req.header('auth-token');
 	if(!token) return res.status(401).json({message: "Access denied!"});
 
@@ -15,21 +15,4 @@ module.exports = function(req, res, next){
 	}
 };
 
-// const jwt = require("jsonwebtoken");
-
-// module.exports = function (req, res, next) {
-//   const authHeader = req.headers["Authorization"];
-//   const token = authHeader && authHeader.split(" ")[1];
-
-//   if (!token) return res.status(401).json({ message: "Access denied!" });
-
-//   jwt.verify(token, process.env.TOKEN_SECRET);
-//   try {
-//     const verified = jwt.verify(token, process.env.TOKEN_SECRET);
-//     // this is gonna give us the payload part
-//     req.user = verified;
-//     next();
-//   } catch (err) {
-//     res.status(400).json({ message: "Invalid Token" });
-//   }
-// };
+module.exports.verifyToken = verifyToken;
